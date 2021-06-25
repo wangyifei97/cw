50834bc5d8f4959c86020db70a1fa01530194adc
Wangyifei <862549425@qq.com>
2021-06-25 09:56:02 +0800

    本地nacos - 注册中心，测试成功

--- demo-nacos-consumer/src/main/java/com/cw/nacos/nacosConsumer/DemoNacosConsumerApplication.java
+++ demo-nacos-consumer/src/main/java/com/cw/nacos/nacosConsumer/DemoNacosConsumerApplication.java
@@ -3,9 +3,10 @@ package com.cw.nacos.nacosConsumer;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@NacosPropertySource(dataId = "nacos-config-consumer", autoRefreshed = true)
+@EnableDiscoveryClient
 public class DemoNacosConsumerApplication {
 
     public static void main(String[] args) {
--- demo-nacos-consumer/src/main/java/com/cw/nacos/nacosConsumer/controller/NacosConfigController.java
+++ demo-nacos-consumer/src/main/java/com/cw/nacos/nacosConsumer/controller/NacosConfigController.java
@@ -1,6 +1,7 @@
 package com.cw.nacos.nacosConsumer.controller;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("config")
 public class NacosConfigController {
-    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+//    @NacosValue(value = "${useLocalCache}", autoRefreshed = true)
+    @Value("${useLocalCache}")
     private boolean useLocalCache;
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
